cf317e4087c7a09cbe2efa300c156f63e458bbdb
543531894@qq.com <543531894@qq.com>
2023-10-27 11:25:37 +0800

    部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
   base: "/threeOne/",
   title: "Hello VuePress",
   description: "Just playing around",
-  theme: "@vuepress/blog",
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
