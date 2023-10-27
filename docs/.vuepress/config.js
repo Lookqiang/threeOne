2c58cbcbc44ada1cb51a1cf61bb0f70c10e5abad
543531894@qq.com <543531894@qq.com>
2023-10-27 10:26:01 +0800

    默认主题配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,16 @@
+import { defaultTheme } from "vuepress";
 module.exports = {
   base: "/threeOne/",
   title: "Hello VuePress",
   description: "Just playing around",
   dest: "public",
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: "首页",
+        link: "/",
+      },
+    ],
+  }),
 };
