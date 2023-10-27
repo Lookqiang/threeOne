b6660a327936c0b13762b4f40b609a9e099dfc70
543531894@qq.com <543531894@qq.com>
2023-10-27 11:04:59 +0800

    默认主题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,11 @@ module.exports = {
   title: "Hello VuePress",
   description: "Just playing around",
   theme: "@vuepress/blog",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "External", link: "https://google.com" },
+    ],
+  },
 };
